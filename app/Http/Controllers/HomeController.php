17a0f154b449968b3017c21ed6cce8fc5b3e1459
J0seCarl0s <jozecarloz_15@hotmail.com>
2018-11-08 21:05:29 -0500

    Terminado inicio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Lava;
 use App\Meeting;
+use App\Doctor;
+use App\Patient;
+use App\Office;
 
 class HomeController extends Controller
 {
@@ -25,39 +28,60 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $anio   = date("Y"); //Obtengo el año actual
-        $meetings = Meeting::where('date','like',$anio.'%')
+        $currentYear   = date("Y"); //Obtengo el año actual
+        $currentMonth  = date("m");
+        $meetings = Meeting::where('date','like',$currentYear.'%')
                 ->get(); //Citas del presente año
+
 
         $values = Lava::DataTable();
         $values ->addDateColumn('Meses')
                 ->addNumberColumn('Asignadas')
                 ->addNumberColumn('Atendidas');
-/*
+
         if($meetings){
-            foreach ($meetings as $meeting) {
-                $values = $values->addRow([,])
+            for ($i=1; $i <= 12; $i++) {
+                $month = $i < 10 ? '0'.$i : $i; //Para que el mes tenga dos cifras 
+                $yearMonth = $currentYear.'/'.$month;
+
+                $meetingsOfCurrentMonth = $meetings->filter(function($val) use ($yearMonth){
+                    return preg_match('@'.$yearMonth.'@', $val->date);
+                });
+                $meetingsAssigned = $meetingsOfCurrentMonth->filter(function($val){
+                    return $val->keyword_state == 1;
+                });
+                $meetingsAttended = $meetingsOfCurrentMonth->filter(function($val){
+                    return $val->keyword_state == 2;
+                });
+
+                $values->addRow([$currentYear.'-'.$month,$meetingsAssigned->count(),$meetingsAttended->count()]);
             }
         }
-                ->addRow(['2004-01', 1000, 400])
-                ->addRow(['2005-01', 1170, 460])
-                ->addRow(['2006-01', 660, 1120])
-                ->addRow(['2007-01', 1030, 54]);
-*/
+                
         Lava::ColumnChart('Finances', $values, [
-            'title' => 'Citas del '.$anio,
+            'title' => 'Citas del '.$currentYear,
             'titleTextStyle' => [
-                'color'    => '#eb6b2c',
+                'color'    => 'black',
                 'fontSize' => 14
-            ]]);
+            ],
+            'height' => 350,
+            'isStacked' => false
+        ]);
             
-        return view('home');
+        return view('home',[
+            'allMeetingsCount'       => Meeting::All()->count(),
+            'meetingsThisMonthCount' => Meeting::where('date','like',$currentYear.'/'.$currentMonth.'%')->count(),
+            'allDoctorsCount'        => Doctor::All()->count(),
+            'allPatientsCount'       => Patient::All()->count(),
+            'allOfficesCount'        => Office::All()->count(),
+            'lastMeetings'           => Meeting::where('id','>=','0')->orderBY('created_at')->take(10)->get(),
+            'lastPatients'           => Patient::where('id','>=','0')->orderBY('created_at')->take(10)->get()
+        ]);
     }
 }
 
-/*
-SELECT COUNT(*),(
-    SELECT CASE WHEN m.date LIKE '2018/01*'THEN '2018-01' WHEN m.date LIKE '2018/02*'THEN '2018-02' WHEN m.date LIKE '2018/03*'THEN '2018-03' WHEN m.date LIKE '2018/04*'THEN '2018-04' WHEN m.date LIKE '2018/05*'THEN '2018-05' WHEN m.date LIKE '2018/06*'THEN '2018-06' WHEN m.date LIKE '2018/07*'THEN '2018-07' WHEN m.date LIKE '2018/08*'THEN '2018-08' WHEN m.date LIKE '2018/09*'THEN '2018-09' WHEN m.date LIKE '2018/10*'THEN '2018-10' WHEN m.date LIKE '2018/11*'THEN '2018-11' WHEN m.date LIKE '2018/12*'THEN '2018-12' ELSE 'nada' END
-)AS month FROM meetings m WHERE date LIKE '2018%'
 
+/*
+SELECT COUNT(*),( 
+    SELECT CASE WHEN m.date LIKE '2018/01%'THEN '2018-01' WHEN m.date LIKE '2018/02%'THEN '2018-02%' WHEN m.date LIKE '2018/03%'THEN '2018-03' WHEN m.date LIKE '2018/04%'THEN '2018-04' WHEN m.date LIKE '2018/05%'THEN '2018-05' WHEN m.date LIKE '2018/06%'THEN '2018-06' WHEN m.date LIKE '2018/07%'THEN '2018-07' WHEN m.date LIKE '2018/08%'THEN '2018-08' WHEN m.date LIKE '2018/09%'THEN '2018-09' WHEN m.date LIKE '2018/10%'THEN '2018-10' WHEN m.date LIKE '2018/11%'THEN '2018-11' WHEN m.date LIKE '2018/12%'THEN '2018-12' ELSE 'nada' END )AS month FROM meetings m WHERE date LIKE '2018%' GROUP BY month
 */
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,141 @@
     <section class="content-header">
       <h1>
         INICIO
-        <small>Optional description</small>
+        <small>Version 1.0</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
+        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
       </ol>
     </section>
 
-    <!-- Main content -->
+    <!-- <MAIN></MAIN>in content -->
     <section class="content container-fluid">
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
-        <div id="perf_div"></div> 
-        @columnchart('Finances', 'perf_div')
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Reporte de citas del <strong>{{date("Y")}}</strong></h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Minimizar">
+                <i class="fa fa-minus"></i></button>
+              
+              </div>
+            </div>
+            <div class="box-body">
+              <div id="perf_div" class="col-md-8"></div> 
+              @columnchart('Finances', 'perf_div')
+              <div class="col-md-4">
+                <div class="info-box bg-purple">
+                  <div class="info-box-icon">
+                    <i class="fa fa-medkit"></i>
+                  </div>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Citas totales</span>
+                    <span class="info-box-number">{{$allMeetingsCount}}</span>
+                    <div class="progress"></div>
+                    <span class="progress-desciption">Citas del mes: <strong>{{$meetingsThisMonthCount}}</strong></span>
+                  </div>
+                </div>
+                <div class="info-box bg-green">
+                  <div class="info-box-icon">
+                    <i class="fa fa-users"></i>
+                  </div>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Pacientes</span>
+                    <div class="progress"></div>
+                    <span class="info-box-number">{{$allPatientsCount}}</span>
+                  </div>
+                </div>
+                <div class="info-box bg-aqua">
+                  <div class="info-box-icon">
+                    <i class="fa fa-user-md"></i>
+                  </div>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Medicos</span>
+                    <div class="progress"></div>
+                    <span class="info-box-number">{{$allDoctorsCount}}</span>
+                  </div>
+                </div>
+                <div class="info-box bg-yellow">
+                  <div class="info-box-icon">
+                    <i class="fa fa-users"></i>
+                  </div>
+                  <div class="info-box-content">
+                    <span class="info-box-text">Consultorios</span>
+                    <div class="progress"></div>
+                    <span class="info-box-number">{{$allOfficesCount}}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3>Últimas citas registradas</h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Minimizar">
+                <i class="fa fa-minus"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                  <tr>
+                    <th>#</th>
+                    <th>Paciente</th>
+                    <th>Fecha</th>
+                    <th>Hora</th>
+                  </tr>
+                  @foreach($lastMeetings as $i => $meeting)
+                  <tr>
+                    <td>{{ $i + 1 }}</td>
+                    <td>{{ $meeting->patient->name.' '.$meeting->patient->lastName }}</td>
+                    <td>{{ $meeting->date }}</td>
+                    <td>{{ $meeting->hour }}</td>
+                  </tr>
+                  @endforeach  
+                </table>  
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="box box-info">
+            <div class="box-header with-border">
+              <h3>Últimos pacientes registrados</h3>
+              <div class="box-tools pull-right">
+                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Minimizar">
+                <i class="fa fa-minus"></i></button>
+              </div>
+            </div>
+            <div class="box-body">
+              <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                  <tr>
+                    <th>#</th>
+                    <th>Paciente</th>
+                    <th>Telefono</th>
+                  </tr>
+                  @foreach($lastPatients as $i => $patient)
+                  <tr>
+                    <td>{{ $i + 1 }}</td>
+                    <td>{{ $patient->name.' '.$patient->lastName }}</td>
+                    <td>{{ $patient->phone }}</td>
+                  </tr>
+                  @endforeach  
+                </table>  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
     <!-- /.content -->
 
