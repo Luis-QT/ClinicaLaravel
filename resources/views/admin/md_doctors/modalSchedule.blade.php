515becc2efd1f4eda174646be9d04a30c24586d8
LuisQT <luis.quispe31@unmsm.edu.pe>
2018-11-21 00:09:10 -0500

    Ahora si

--- resources/views/admin/md_doctors/modalSchedule.blade.php
+++ resources/views/admin/md_doctors/modalSchedule.blade.php
@@ -13,39 +13,73 @@
               <button type="button" id="btnAgregarHorario" class="btn btn-success btn-sm" data-count="0"><span class="fa fa-plus">Agregar Horario</span></button>
               
               <div id="contenedor">
-
-                <hr>
-                <div class="row div-horario">
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label>Dia</label>
-                      <select class="form-control select2 day" style="width: 100%;">
-                        <option value="">--Selec--</option>
-                        <option value="1">Lunes</option>
-                        <option value="2">Martes</option>
-                        <option value="3">Miercoles</option>
-                        <option value="4">Jueves</option>
-                        <option value="5">Viernes</option>
-                        <option value="6">Sabado</option>
-                        <option value="7">Domingo</option>
-                      </select>
+                @if($doctor->schedules->isEmpty())
+                  <hr>
+                  <div class="row div-horario">
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Dia</label>
+                        <select class="form-control select2 day" style="width: 100%;">
+                          <option value="">--Selec--</option>
+                          <option value="1">Lunes</option>
+                          <option value="2">Martes</option>
+                          <option value="3">Miercoles</option>
+                          <option value="4">Jueves</option>
+                          <option value="5">Viernes</option>
+                          <option value="6">Sabado</option>
+                          <option value="7">Domingo</option>
+                        </select>
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label>Hora Inicio</label>
-                      <input class="form-control datepicker-justHour startHour" type="text"></input>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Hora Inicio</label>
+                        <input class="form-control datepicker-justHour startHour" type="text"></input>
+                      </div>
                     </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label>Hora Fin</label>
-                      <input class="form-control datepicker-justHour endHour" type="text"></input>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Hora Fin</label>
+                        <input class="form-control datepicker-justHour endHour" type="text"></input>
+                      </div>
                     </div>
+                    <button type="button" class="btn btn-danger btn-sm" data-count="0"><span class="fa fa-remove"></span></button>
                   </div>
-                  <button type="button" class="btn btn-danger btn-sm" data-count="0"><span class="fa fa-remove"></span></button>
-
-                </div>
+                @else
+                  @foreach($doctor->schedules as $schedule)
+                  <hr>
+                  <div class="row div-horario">
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Dia</label>
+                        <select class="form-control select2 day" style="width: 100%;">
+                          <option value="">--Selec--</option>
+                          <option value="1" @if($schedule->day_of_week == "1") selected @endif >Lunes</option>
+                          <option value="2" @if($schedule->day_of_week == "2") selected @endif >Martes</option>
+                          <option value="3" @if($schedule->day_of_week == "3") selected @endif >Miercoles</option>
+                          <option value="4" @if($schedule->day_of_week == "4") selected @endif >Jueves</option>
+                          <option value="5" @if($schedule->day_of_week == "5") selected @endif >Viernes</option>
+                          <option value="6" @if($schedule->day_of_week == "6") selected @endif >Sabado</option>
+                          <option value="7" @if($schedule->day_of_week == "7") selected @endif >Domingo</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Hora Inicio</label>
+                        <input class="form-control datepicker-justHour startHour" value="{{ $schedule->arrival_time }}" type="text"></input>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-group">
+                        <label>Hora Fin</label>
+                        <input class="form-control datepicker-justHour endHour" value="{{ $schedule->quitting_time }} type="text"></input>
+                      </div>
+                    </div>
+                    <button type="button" class="btn btn-danger btn-sm" data-count="0"><span class="fa fa-remove"></span></button>
+                  </div>
+                  @endforeach
+                @endif
               </div>
               <div class="row">
                 <div class="form-group text-center">
